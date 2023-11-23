eacce8a29df92c300f99a5360daa627e209a21bb
Sebastian Rueda <jrueda03@gmail.com>
2023-11-23 00:07:41 -0500

    Added Project files

---
+++ Source/SantasToyWarfare/SantasToyWarfarePlayerController.h
@@ -0,0 +1,31 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SantasToyWarfarePlayerController.generated.h"
+
+class UInputMappingContext;
+
+/**
+ *
+ */
+UCLASS()
+class SANTASTOYWARFARE_API ASantasToyWarfarePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+protected:
+
+	/** Input Mapping Context to be used for player input */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* InputMappingContext;
+
+	// Begin Actor interface
+protected:
+
+	virtual void BeginPlay() override;
+
+	// End Actor interface
+};
